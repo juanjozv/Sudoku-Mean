1d47ba0e52bb69b9eab7b66b74493f2a004f63bb
Juanjo <juanjozv@gmail.com>
2017-10-03 01:53:45 -0600

    Changes in db sudoku schema

--- db/models/sudokuModel.js
+++ db/models/sudokuModel.js
@@ -3,8 +3,9 @@ let Schema = mongoose.Schema;
 
 let SudokuSchema = new Schema({
     user: String,
-    lastPlayed: Date, // to obtain the last played sudoku by the user.
-    playableSudoku: [{ x: Number, y: Number, value: Number }]
+    difficulty: String, //normal, easy, hard, random(generated)
+    lastPlayed: Date, // to obtain the last played sudoku game by the user.
+    playableSudoku: [{ x: Number, y: Number, value: Number, isClue: Boolean }]
 });
 
 module.exports = mongoose.model('Sudoku', SudokuSchema);
