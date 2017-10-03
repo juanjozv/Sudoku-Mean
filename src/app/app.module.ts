7d832e036288b1e68fb931104f3d8eeb12be71fe
Leo <leonelgodinez15@hotmail.es>
2017-10-02 21:13:58 -0600

    Options component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SudokuComponent } from './components/sudoku/sudoku.component';
+import { OptionsComponent } from './components/options/options.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SudokuComponent
+    SudokuComponent,
+    OptionsComponent
   ],
   imports: [
     BrowserModule
