d5308e6ae984255d1440b7a1dcb850010fb760cd
Juanjo <juanjozv@gmail.com>
2017-10-05 16:20:39 -0600

    Deleting useless matrix attribute in sudoku.component

--- src/app/components/sudoku/sudoku.component.ts
+++ src/app/components/sudoku/sudoku.component.ts
@@ -9,7 +9,6 @@ import { SudokuService } from '../../services/sudoku.service';
   providers: [SudokuService, SudokuView]
 })
 export class SudokuComponent implements OnInit {
-  matrix: any
   _sudokuView: any = new SudokuView();
   constructor(private _sudokuService: SudokuService) {
   }
@@ -19,20 +18,17 @@ export class SudokuComponent implements OnInit {
   }
 
   createNewSudoku() {
-    this._sudokuService.getNewSudoku('leo', 'normal', 'hoy').subscribe(res => { this.paintSudokuView(res) });
+    this._sudokuService.getNewSudoku('leo', 'normal', 'hoy').subscribe(res => { this.paintSudokuView(res); });
   }
-  
+
   paintSudokuView(res_) {
     console.log(res_);
-    this.matrix = res_;
     let newMatrix = Array.from({ length: 9 }, () => new Array(9));
     console.log(newMatrix);
-    this.matrix.playableSudoku.forEach(elem => {
+    res_.playableSudoku.forEach(elem => {
       newMatrix[elem.x][elem.y] = elem.value;
-      // agregar pistas
     });
-    this.matrix = newMatrix;
-    console.log(this.matrix);
-    this._sudokuView.paint(this.matrix);
+    console.log(newMatrix);
+    this._sudokuView.paint(newMatrix);
   }
 }
