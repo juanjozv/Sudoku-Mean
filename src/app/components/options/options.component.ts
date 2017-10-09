814de79fbb83837b8dd7d943c9dd5854f3fb227f
Juanjo <juanjozv@gmail.com>
2017-10-09 01:31:31 -0600

    Deleting db model

--- src/app/components/options/options.component.ts
+++ src/app/components/options/options.component.ts
@@ -13,18 +13,23 @@ import $ from 'jquery';
 })
 export class OptionsComponent implements OnInit {
   _userOptionsView: any = new UserOptionsView();
-  constructor(private _sudokuService: SudokuService,private _sudokuComponent: SudokuComponent) { }
+  constructor(private _sudokuService: SudokuService, private _sudokuComponent: SudokuComponent) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.loadSudokuInMatrix('ee');
+   }
 
   loadSudokus() {
     let user = this._userOptionsView.getUser();
-    if (user != "empty")
-      this._sudokuService.loadUserSudokus(user).subscribe(res => { this._userOptionsView.showSudokus(res) });
+    if (user != "empty") {
+      this._sudokuService.loadUserSudokus(user).subscribe(res => { this._userOptionsView.showSudokus(res); });
+    }
   }
-  loadSudokuInMatrix(id) { 
-    console.log(id);
-    this._sudokuService.loadSudoku(id).subscribe(res => { this._sudokuComponent.paintSudokuView(res)});
+  loadSudokuInMatrix(id) {
+    if(id != 'ee') {
+      console.log(id);
+      this._sudokuService.loadSudoku(id).subscribe(res => { this._sudokuComponent.paintSudokuView(res); });
+    }
   }
 }
 
